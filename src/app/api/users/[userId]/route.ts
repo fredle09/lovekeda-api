64835c73d79fb8554e9823c01531223c0c7c9f6d
fredle09 <le197903@gmail.com>
2025-01-20 01:04:55 +0700

    feat: add GET endpoint to retrieve user by userId with error handling

---
+++ src/app/api/users/[userId]/route.ts
@@ -0,0 +1,17 @@
+import { User } from "@/lib/models";
+import { badRequestResponse, errorResponse, successfulResponse } from "@/utils/handlers";
+import { NextRequest } from "next/server";
+
+export const GET = async (req: NextRequest, { params }: { params: Awaited<{ userId: string }> }) => {
+  const { userId } = await params;
+  if (!userId) return badRequestResponse({ message: "userId is required" });
+  try {
+    const user = await User.findOne({ user_id: userId });
+    if (!user) return badRequestResponse({ message: "user not found" });
+
+    return successfulResponse({ data: user });
+  } catch (error: any) {
+    console.log("🚀 ~ @GET /users/:userId ~ error:", error.message);
+    return errorResponse({ message: error?.message });
+  }
+}
